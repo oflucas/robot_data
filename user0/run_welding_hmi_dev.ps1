0b7b679dd21af32cb5ba05bd17bd79f371d1a0c8
Lucas Li <scofieldgood@gmail.com>
2024-10-15 23:59:41 +0800

    Rearrange files

---
+++ user0/run_welding_hmi_dev.ps1
@@ -0,0 +1,13 @@
+Write-Host "Current Directory: $(Get-Location)"
+$base = $env:ROBOT_WORKSPACE
+$dev_repo = "robot"
+$dist = "dist_robot"
+$exec_path = Join-Path -Path $base -ChildPath $dev_repo
+Write-Host $exec_path
+
+Set-Location -Path $exec_path
+Write-Host "cd Directory: $(Get-Location)"
+
+python --version
+python -m robot.welding_hmi --debug_mode --username user0
+$name = Read-Host "Enter your name"
